2ddda523169329102ce10342f4663da3c8bcfd81
Alexssandre Vieira Carvalho <alexvieiracarvalho@gmail.com>
2025-03-22 17:41:17 -0300

    Chore: Configure entry point for execution

---
+++ main.py
@@ -0,0 +1,6 @@
+from src.main import main
+
+# This conditional statement checks if the script is being run as the main program.
+if __name__ == "__main__":
+    # If the script is the main program, call the main() function from the src.main module.
+    main()
